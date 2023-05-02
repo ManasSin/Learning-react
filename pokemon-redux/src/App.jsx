053e94378a61851165b5dff6d89cb827d40cecdb
ManasSin <maltisadan12@gmail.com>
2023-05-02 08:51:29 +0530

    Pokemon APP with redux toolkit

---
+++ pokemon-redux/src/App.jsx
@@ -0,0 +1,13 @@
+import SearchBox from "./Components/SearchBox";
+import PokemonList from "./Components/PokemonList";
+
+function App() {
+  return (
+    <>
+      <SearchBox />
+      <PokemonList />
+    </>
+  );
+}
+
+export default App;
