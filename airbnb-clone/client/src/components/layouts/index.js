8e372b45f534c9da12dfdf18f568ac085f029735
ManasSin <maltisadan12@gmail.com>
2023-07-07 17:49:15 +0530

    Stop tracking few files, removed them

--- airbnb-clone/client/src/components/layouts/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Search } from "./Search";
-export { default as ModalUniversal } from "./ModalUniversal";
-export { default as UserMenu } from "./UserMenu";
-export { default as Button } from "./Button";
--- airbnb-clone/client/src/components/templates/index.js
+++
@@ -1,3 +0,0 @@
-export { default as Header } from "./Header";
-export { default as FilterBar } from "./FilterBar";
-export { default as AuthModal } from "./AuthModal";
